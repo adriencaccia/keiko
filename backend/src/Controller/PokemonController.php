a5c1c2ed44412539bb3362cc031bc33ccab042df
Cacciaguerra Adrien <adrien.caccia@gmail.com>
2019-06-26 23:06:00 +0200

    Add hardcoded pokemon entity at /pokemon

--- backend/src/Controller/PokemonController.php
+++ backend/src/Controller/PokemonController.php
@@ -2,16 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Pokemon;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class PokemonController
 {
+    private $normalizer;
+
+    public function __construct(NormalizerInterface $normalizer)
+    {
+        $this->normalizer = $normalizer;
+    }
+
     /**
      * @Route("/pokemon")
      */
-    public function helloWorld()
+    public function get()
     {
-        return new Response("Hello World");
+        $pokemon = new Pokemon();
+        $pokemon->setName("bulbasaur");
+        $pokemon->setWeight(67);
+        $pokemon->setHeight(67);
+
+        $serialized_pokemon = $this->normalizer->normalize($pokemon, "json");
+
+        return new JsonResponse($serialized_pokemon);
     }
 }
