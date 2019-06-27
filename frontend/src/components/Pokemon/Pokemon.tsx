988586fefe2e60728ea08294a0f2bfe38581af01
Cacciaguerra Adrien <adrien.caccia@gmail.com>
2019-06-27 20:17:55 +0200

    Add pokemon attributes traduction

--- frontend/src/components/Pokemon/Pokemon.tsx
+++ frontend/src/components/Pokemon/Pokemon.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { FormattedMessage } from 'react-intl';
 
 interface Props {
   id: number;
@@ -10,8 +11,12 @@ class Pokemon extends React.Component<Props> {
     const { id, name } = this.props;
     return (
       <div>
-        <div>name: {name}</div>
-        <div>id: {id}</div>
+        <div>
+          <FormattedMessage id="pokemon.id" />: {id}
+        </div>
+        <div>
+          <FormattedMessage id="pokemon.name" />: {name}
+        </div>
         <img
           src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
         />
