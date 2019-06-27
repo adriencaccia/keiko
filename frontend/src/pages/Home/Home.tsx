2aef01dd6f4ea1443c859bcabb8445bb72e14dc8
Cacciaguerra Adrien <adrien.caccia@gmail.com>
2019-06-27 20:01:00 +0200

    Retrieve a pokemon from the API

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,11 +1,30 @@
 import Pokemon from 'components/Pokemon';
 import * as React from 'react';
+import { makeGetRequest } from 'services/networking/request';
 import Style from './Home.style';
 
-class Home extends React.Component {
+interface Props {}
+interface State {
+  pokemons: Array<{
+    id: number;
+    name: string;
+  }>;
+}
+
+class Home extends React.Component<Props, State> {
+  state: State = {
+    pokemons: [],
+  };
+
+  async componentDidMount() {
+    const { body: pokemons } = await makeGetRequest('/pokemon');
+    this.setState({ pokemons });
+  }
   render(): React.ReactNode {
+    const pokemon = this.state.pokemons && this.state.pokemons[0];
     return (
       <Style.Intro>
+        {pokemon && <Pokemon name={pokemon.name} id={pokemon.id} />}
         <Pokemon name="Carapuce" id={7} />
         <Pokemon name="Carabaffe" id={8} />
         <Pokemon name="Tortank" id={9} />
