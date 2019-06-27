f514e45cfccdd32008b325e733aa14977578b098
Cacciaguerra Adrien <adrien.caccia@gmail.com>
2019-06-27 19:01:03 +0200

    Add Ability entity, migrate it and expose via ApiPlatform

---
+++ backend/src/Entity/Ability.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * @ApiResource
+ * @ORM\Entity()
+ * @UniqueEntity("name")
+ */
+class Ability
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     * @Assert\NotBlank
+     */
+    private $name;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+}
