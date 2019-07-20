c9dd83172cec8ed84e498b6a2c010e6596eb5724
Cacciaguerra Adrien <adrien.caccia@gmail.com>
2019-07-20 22:21:00 +0200

    ✅ Add Pokemon normalizer tests

---
+++ frontend/src/redux/Pokemon/__tests__/normalizer.test.ts
@@ -0,0 +1,24 @@
+import { PokemonInterface } from 'components/Pokemon/Pokemon';
+import { normalize } from 'services/PokemonNormalizer';
+
+const pokemonArray = [
+  { id: 1, name: 'bulbasaur', height: 7, weight: 69 },
+  { id: 2, name: 'ivysaur', height: 10, weight: 130 },
+];
+
+describe('Pokemon normalizer', () => {
+  describe('empty array case', () => {
+    it('Should return an empty object', () => {
+      expect(normalize([])).toEqual({});
+    });
+  });
+
+  describe('filled array case', () => {
+    it('Should return a filled object', () => {
+      const pokemons = normalize(pokemonArray);
+      pokemonArray.forEach((pokemon: PokemonInterface) => {
+        expect(pokemons[pokemon.id]).toEqual(pokemon);
+      });
+    });
+  });
+});
--- frontend/src/services/PokemonNormalizer.ts
+++ frontend/src/services/PokemonNormalizer.ts
@@ -1,7 +1,7 @@
 import { PokemonInterface } from 'components/Pokemon/Pokemon';
 import { PokemonState } from 'redux/Pokemon';
 
-const normalize = (array: PokemonInterface[]) =>
+const normalize = (array: PokemonInterface[]): PokemonState =>
   array.reduce(
     (object: PokemonState, item: PokemonInterface) => ({ ...object, [item.id]: item }),
     {},
